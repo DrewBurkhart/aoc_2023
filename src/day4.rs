5122f9aeb3f46b0d782af0be0cecd7927b7e1292
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-12-04 10:35:35 -0700

    Optimize day 4

--- src/day4.rs
+++ src/day4.rs
@@ -5,11 +5,12 @@ struct Game<'a> {
 
 pub(crate) fn problem1() {
     let input = std::fs::read_to_string("inputs/input4.txt").expect("should've been able to read");
-    let sum: usize = input
-        .lines()
-        .map(|l| extract_game(l))
-        .map(|game| {
-            let win_count = score_game(game);
+    let lines: Vec<&str> = input.lines().collect();
+    let sum: usize = lines
+        .iter()
+        .map(|&l| {
+            let game = extract_game(l);
+            let win_count = score_game(&game);
             if win_count > 1 {
                 2usize.pow(win_count as u32 - 1)
             } else {
@@ -23,17 +24,16 @@ pub(crate) fn problem1() {
 
 fn extract_game(l: &str) -> Game {
     let (_id, nums) = l.split_once(":").expect("should've had game id");
-    let parts: Vec<&str> = nums.split('|').map(|s| s.trim()).collect();
-
-    let player_numbers: Vec<&str> = parts[0].split_whitespace().collect();
-    let winning_numbers: Vec<&str> = parts[1].split_whitespace().collect();
+    let (player_numbers, winning_numbers) = nums
+        .split_once('|')
+        .expect("should've had player and winning numbers");
     Game {
-        player_numbers,
-        winning_numbers,
+        player_numbers: player_numbers.trim().split_whitespace().collect(),
+        winning_numbers: winning_numbers.trim().split_whitespace().collect(),
     }
 }
 
-fn score_game(game: Game) -> usize {
+fn score_game(game: &Game) -> usize {
     game.player_numbers
         .iter()
         .filter(|&&item| game.winning_numbers.contains(&item))
@@ -42,13 +42,12 @@ fn score_game(game: Game) -> usize {
 
 pub(crate) fn problem2() {
     let input = std::fs::read_to_string("inputs/input4.txt").expect("should've been able to read");
-    let lines = input.lines().collect::<Vec<&str>>();
-    println!("{}", lines.len());
+    let lines: Vec<&str> = input.lines().collect();
     let mut matrix = vec![1; lines.len()];
 
-    for (i, l) in lines.iter().enumerate() {
+    for (i, &l) in lines.iter().enumerate() {
         let game = extract_game(l);
-        for x in 1..=score_game(game) {
+        for x in 1..=score_game(&game) {
             matrix[i + x] += matrix[i];
         }
     }
