341e5bcc45ea2528b2666949ef264bc8bd4da34d
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-12-14 08:27:10 -0700

    Solution - Day 14 / Puzzle 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 pub(crate) fn problem1() {
     let input = fs::read_to_string("inputs/input14.txt").expect("Failed to read input file");
@@ -40,6 +40,37 @@ fn calculate_load(platform: &Vec<Vec<char>>) -> usize {
         })
         .sum()
 }
+
+fn rotate_platform(platform: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut rotated_platform = vec![vec!['.'; platform.len()]; platform[0].len()];
+    for row in 0..platform.len() {
+        for col in 0..platform[0].len() {
+            rotated_platform[col][platform.len() - 1 - row] = platform[row][col];
+        }
+    }
+    rotated_platform
+}
+
 pub(crate) fn problem2() {
-    println!("not implemented");
+    let input = fs::read_to_string("inputs/input14.txt").expect("Failed to read input file");
+    let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
+    let mut platform: Vec<Vec<char>> = lines.iter().map(|line| line.chars().collect()).collect();
+
+    let mut memory = HashMap::new();
+    for i in 1..1000000000 {
+        for _ in 0..4 {
+            tilt_platform_north(&mut platform);
+            platform = rotate_platform(&platform);
+        }
+        // If we've seen this iteration of the platform before, check if it is
+        // evenly divisible by the number of checks remaining. If it is, we can
+        // stop iterating.
+        if let Some(seen_at) = memory.insert(platform.clone(), i) {
+            if (1000000000 - i) % (i - seen_at) == 0 {
+                break;
+            }
+        }
+    }
+
+    println!("{}", calculate_load(&platform));
 }
