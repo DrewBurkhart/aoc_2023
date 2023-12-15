993bcefa0d5316a082d21a21f712eb8de6d0bec0
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-12-15 13:30:45 -0700

    Solution - Day 15 / Puzzle 1

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,22 @@
+use std::fs;
+
 pub(crate) fn problem1() {
-    println!("not implemented");
+    let input = fs::read_to_string("inputs/input15.txt").expect("should've been able to read");
+
+    let sum: u64 = input
+        .split(',')
+        .map(|string| {
+            let cleaned = string
+                .chars()
+                .filter(|&char| char != ' ' && char != '\n')
+                .collect::<String>();
+            cleaned
+                .chars()
+                .fold(0, |acc, char| ((acc + char as u64) * 17) % 256)
+        })
+        .sum();
+
+    println!("{}", sum);
 }
 
 pub(crate) fn problem2() {
