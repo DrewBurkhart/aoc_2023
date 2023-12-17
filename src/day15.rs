3ea1a24fdfb73a29018949cd0170bbb8374cbaa0
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-12-17 08:09:24 -0700

    Solution - Day 16 / Puzzle 2

--- src/day15.rs
+++ src/day15.rs
@@ -20,5 +20,54 @@ pub(crate) fn problem1() {
 }
 
 pub(crate) fn problem2() {
-    println!("not implemented");
+    let input = fs::read_to_string("inputs/input15.txt").expect("should've been able to read");
+    let mut boxes = vec![vec![]; 256];
+
+    for instruction in input.trim().split(',') {
+        if instruction.ends_with('-') {
+            let key = instruction[0..instruction.len() - 1].to_string();
+            let h = hash(&key);
+            if let Some(position) = boxes[h].iter().position(|v: &Lens| v.key == key) {
+                boxes[h].remove(position);
+            }
+        } else {
+            let (key, value) = instruction.split_once('=').unwrap();
+            let value = value.parse().unwrap();
+            let key = key.to_string();
+            let h = hash(&key);
+            if let Some(position) = boxes[h].iter().position(|v| v.key == key) {
+                boxes[h][position].value = value;
+            } else {
+                boxes[h].push(Lens {
+                    key: key.to_string(),
+                    value,
+                });
+            }
+        }
+    }
+
+    println!(
+        "{}",
+        boxes
+            .into_iter()
+            .enumerate()
+            .map(|(i, b)| {
+                (i + 1)
+                    * b.into_iter()
+                        .enumerate()
+                        .map(|(k, lens)| ((k + 1) * lens.value))
+                        .sum::<usize>()
+            })
+            .sum::<usize>()
+    );
+}
+
+fn hash(s: &str) -> usize {
+    s.chars().fold(0, |acc, c| ((acc + c as usize) * 17) % 256)
+}
+
+#[derive(Clone, Debug)]
+struct Lens {
+    key: String,
+    value: usize,
 }
