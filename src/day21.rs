4e781c58866e0c955fc86db5b6b79cd6d071adfb
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-12-22 15:10:33 -0700

    Broken Solution - Day 21 / Puzzle 2

--- src/day21.rs
+++ src/day21.rs
@@ -20,8 +20,16 @@ fn breadth_first_search(grid: &[&[u8]], start: (isize, isize), steps: usize) -> 
     pos.len()
 }
 
-pub(crate) fn problem1() {
-    let input = fs::read_to_string("inputs/input21.txt").expect("should've been able to read");
+fn find_polynomial_length(grid: &[&[u8]], start: (isize, isize), target_step: usize) -> usize {
+    let n1 = breadth_first_search(grid, start, target_step % grid.len() + grid.len() * 0);
+    let n2 = breadth_first_search(grid, start, target_step % grid.len() + grid.len() * 1);
+    let n3 = breadth_first_search(grid, start, target_step % grid.len() + grid.len() * 2);
+    let target_step = target_step / grid.len();
+    let [a, b, c] = [n1, n2 - n1, n3 - n2];
+    return a + b * target_step + (target_step * (target_step - 1) / 2) * (c - b);
+}
+
+fn get_grid_and_start(input: &str) -> (Vec<&[u8]>, (isize, isize)) {
     let grid = input
         .trim()
         .split('\n')
@@ -32,9 +40,17 @@ pub(crate) fn problem1() {
         .find(|&(r, c)| grid[r][c] == b'S')
         .map(|(r, c)| (r as isize, c as isize))
         .unwrap();
+    (grid, start)
+}
+
+pub(crate) fn problem1() {
+    let input = fs::read_to_string("inputs/input21.txt").expect("should've been able to read");
+    let (grid, start) = get_grid_and_start(&input);
     println!("{}", breadth_first_search(&grid, start, 64));
 }
 
 pub(crate) fn problem2() {
-    println!("not implemented");
+    let input = fs::read_to_string("inputs/input21.txt").expect("should've been able to read");
+    let (grid, start) = get_grid_and_start(&input);
+    println!("{}", find_polynomial_length(&grid, start, 26501365));
 }
